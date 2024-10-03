9f53dee05386a86aa30410c940fb86700398483e
Raul Gonzales <ragonz1108@gmail.com>
2024-10-03 12:10:57 -0500

    Fixed bug in checkWin if conditions

--- script.js
+++ script.js
@@ -28,22 +28,40 @@ function gameBoard() {
     checkWin(playerMark) {
       for (let i = 0; i < 3; i++) {
         // check rows
-        if (playerMark === (board[0][i] && board[1][i] && board[2][i])) {
+        if (
+          board[0][i] &&
+          board[1][i] &&
+          board[2][i] &&
+          board[0][i] === playerMark &&
+          board[1][i] === playerMark &&
+          board[2][i] === playerMark
+        ) {
           return true;
         }
         // check columns
-        if (playerMark === (board[i][0] && board[i][1] && board[i][2])) {
-          return true;
-        }
-        // check diagonals
         if (
-          playerMark === (board[0][0] && board[1][1] && board[2][2]) ||
-          playerMark === (board[0][2] && board[1][1] && board[2][0])
+          board[i][0] &&
+          board[i][1] &&
+          board[i][2] &&
+          board[i][0] === playerMark &&
+          board[i][1] === playerMark &&
+          board[i][2] === playerMark
         ) {
           return true;
         }
       }
 
+      // check diagonals
+      if (
+        board[0][0] &&
+        board[1][1] &&
+        board[2][2] &&
+        board[0][0] === playerMark &&
+        board[1][1] === playerMark &&
+        board[2][2] === playerMark
+      ) {
+        return true;
+      }
       return false;
     },
     reset() {
@@ -124,7 +142,6 @@ function startGame() {
 
   console.log("It's a tie!");
 }
-
 function playGame() {
   try {
     startGame();
